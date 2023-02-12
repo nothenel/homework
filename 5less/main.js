da7823f8df453081e5ce588b5208e153567c0907
nothenel <ermekovkayrat@gmail.com>
2023-02-12 14:27:51 +0600

    5-hw

---
+++ 5less/main.js
@@ -0,0 +1,73 @@
+
+
+
+//                                 task 1
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+    student: true
+}
+let sum = salaries.John + salaries.Ann + salaries.Pete
+console.log(sum)
+
+
+//                                                                      task 2
+let guy = {
+    name: 'Ivan',
+    age: 26
+}
+console.log(guy.name)
+
+
+//                                                  task 3
+guy.age = guy.age + 5
+
+
+//                                                 task 4
+guy.student = false
+
+//                                                 task 5
+console.log('BRAC' in guy)
+
+//                                                  task 6
+//                                                 НЕ СМОГ
+
+
+//                                                task 7
+let guy2 = Object.assign({},guy)
+guy2.name = 'Kayrat'
+console.log(guy)
+console.log(guy2)
+
+
+//                                                                            task 8
+let cars = ['bmw', 'honda', 'mercedes', 'lexus']
+cars.splice(3)
+
+
+//                                       task 9
+console.log(cars[3])
+
+
+//                                       task 10
+cars[1] = 'toyota'
+
+
+//                             task 11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
